75e83e9e858fde2c2a7b1e5682ceb3082a7eda1a
kajetan-grabara-grabar <kajetan.grabara@gmail.com>
2024-02-18 20:46:10 +0100

    delete sleep

--- docker/build.sh
+++ docker/build.sh
@@ -1,10 +1,22 @@
 #!/bin.sh
 MODEL=$1
+
+echo '--------------'
+echo 'Installing dependencies'
+echo '--------------'
+
+apt update
+apt install -y curl
+
+echo '--------------'
+echo 'Dowloading LLM model'
+echo '--------------'
+
 ollama serve &
 curl --connect-timeout 5 \
     --max-time 10 \
     --retry 5 \
     --retry-delay 0 \
     --retry-max-time 40 \
-    'http://your_urlhttp://localhost:11434'
+    'http://localhost:11434'
 ollama pull $MODEL
